4879f4b45cc4dd3217cd5b11ee872244f8bbe164
ISBrazil <renan.cardoso@mercadolivre.com>
2022-07-31 19:11:38 -0300

    ensure encripty calls correct values

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,13 +1,29 @@
- class Encrypter {
-    async compare(password, hashPassword) {
-        return true
-    }
- }
+const bcrypt = require('bcrypt') 
+class Encrypter {
+async compare(value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
+}
+}
  
- describe('Encrypter', () => {
+describe('Encrypter', () => {
     test('Shoul return true if bcrypt returns true', async () => {
         const sut = new Encrypter()
-        const isValid = await sut.compare('any_password', 'dash_password')
+        const isValid = await sut.compare('any_password', 'hash_password')
         expect(isValid).toBe(true)
     })
- })
+
+    test('Shoul return false if bcrypt returns false', async () => {
+        const sut = new Encrypter()
+        bcrypt.isValid = false
+        const isValid = await sut.compare('any_password', 'hash_password')
+        expect(isValid).toBe(false)
+    })
+
+    test('Shoul call bcrypt with correct values', async () => {
+        const sut = new Encrypter()
+        const isValid = await sut.compare('any_password', 'hash_password')
+        expect(bcrypt.value).toBe('any_password')
+        expect(bcrypt.hash).toBe('hash_password')
+    })
+})
